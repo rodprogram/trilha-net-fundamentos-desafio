f0987d70bd29f2ac0bd135e351c035ebbbf34800
Rodrigo Almirante <rodrigoprogram@gmail.com>
2023-11-21 23:38:47 -0300

    Desafio do App Estacionamento Concluído! - O desafio consiste em criar funções básicas de pesquisa, inserção de dado do veículo e cálculo de custos.

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -14,8 +14,6 @@ namespace DesafioFundamentos.Models
 
         public void AdicionarVeiculo()
         {
-            // TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veiculos"
-            // *IMPLEMENTE AQUI*
             Console.WriteLine("Digite a placa do veículo para estacionar:");            
             veiculos.Add(Console.ReadLine());
         }
@@ -23,26 +21,16 @@ namespace DesafioFundamentos.Models
         public void RemoverVeiculo()
         {          
 
-            // Pedir para o usuário digitar a placa e armazenar na variável placa
-            // *IMPLEMENTE AQUI*
-
             Console.WriteLine("Digite a placa do veículo: ");;
             string placa = Console.ReadLine();
 
             // Verifica se o veículo existe
             if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
             {
-                
-                // TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado,
-                // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal                
-                // *IMPLEMENTE AQUI*
 
                 Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado: ");
                 int horas = Convert.ToInt16(Console.ReadLine());
                 decimal valorTotal = precoInicial + precoPorHora * horas; 
-
-                // TODO: Remover a placa digitada da lista de veículos
-                // *IMPLEMENTE AQUI*
 
                 veiculos.Remove(placa);
                 Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: {valorTotal:c}");
@@ -60,9 +48,7 @@ namespace DesafioFundamentos.Models
             {
                 Console.WriteLine("Os veículos estacionados são:");
                
-                // TODO: Realizar um laço de repetição, exibindo os veículos estacionados
-                // *IMPLEMENTE AQUI*
-                foreach(var v in veiculos)
+                 foreach(var v in veiculos)
                 {
                     Console.WriteLine(v);
                 }
